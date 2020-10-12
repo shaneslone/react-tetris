4f2268df473b2515de1c11f2e4cce70305696b1d
Shane Slone <slone.shane@gmail.com>
2020-10-11 20:58:07 -0400

    First commit

---
+++ src/components/Tetris.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { createStage } from '../gameHelpers'
+
+// Components
+import Stage from './Stage'
+import Display from './Display'
+import StartButton from './StartButton'
+
+
+export default function Tetris() {
+    return (
+        <div>
+            <Stage stage={createStage()} />
+            <aside>
+                <div>
+                <Display text="Score"/>
+                <Display text="Rows"/>
+                <Display text="Level"/>
+                </div>
+                <StartButton />
+            </aside>
+            
+        </div>
+    )
+}

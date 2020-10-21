8240d1c5439884839359d71a34f1e5e377cf9e3f
Shane Slone <slone.shane@gmail.com>
2020-10-21 14:34:11 -0400

    added collision and movement

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { createStage, checkCollision } from '../gameHelpers'
+
 //styled components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
 
@@ -16,15 +18,56 @@ export default function Tetris() {
     const [dropTime, setDropTime] = useState(null)
     const [gameOver, setGameOver] = useState(false)
 
-    const [player] = usePlayer()
-    const [stage, setStage] = useStage(player)
+    const [player, updatePlayerPos, resetPlayer] = usePlayer()
+    const [stage, setStage] = useStage(player, resetPlayer)
 
 
     console.log('re-render')
 
+    const movePlayer = dir => {
+        if(!checkCollision(player, stage, { x: dir, y: 0})){
+            updatePlayerPos({ x: dir, y: 0})
+        }
+    }
+    const startGame = () => {
+        console.log('test')
+        // reset everything
+        setStage(createStage())
+        resetPlayer()
+        setGameOver(false)
+    }
+    const drop = () => {
+        if(!checkCollision(player, stage, { x: 0, y: 1 })){
+            updatePlayerPos({ x: 0, y: 1, collided: false })
+        } else {
+            //Game Over
+            if(player.pos.y < 1){
+                console.log('GAME OVER')
+                setGameOver(true)
+                setDropTime(null)
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true})
+        }
+    }
+    const dropPlayer = () => {
+        drop()
+    }
+
+    const move =  ({ keyCode }) => {
+        if(!gameOver){
+            if(keyCode === 37){
+                movePlayer(-1)
+            } else if(keyCode === 39) {
+                movePlayer(1)
+            } else if(keyCode === 40) {
+                dropPlayer()
+            }
+        }
+    }
+
     return (
         <StyledTetrisWrapper>
-            <StyledTetris>
+            <StyledTetris role='button' tabIndex='0' onKeyDown={e => move(e)}>
             <Stage stage={stage} />
             <aside>
                 {gameOver ? (
@@ -36,7 +79,7 @@ export default function Tetris() {
                 <Display text="Level"/>
                 </div>
                 )}
-                <StartButton />
+                <StartButton callback={startGame}/>
             </aside>
             </StyledTetris> 
         </StyledTetrisWrapper>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { createStage } from '../gameHelpers'
 
-export const useStage = () => {
-    const [stage, setState] = useState(createStage())
+export const useStage = (player, resetPlayer) => {
+    const [stage, setStage] = useState(createStage())
 
-    return [stage, setState]
+    useEffect(() => {
+        const updateStage = prevStage => {
+            //first flush stage
+            const newStage = prevStage.map(row =>
+                    row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+                )
+
+            // then draw the tetromino
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if(value !== 0){
+                        newStage[y + player.pos.y][x + player.pos.x] = [
+                            value,
+                            `${player.collided ? 'merged' : 'clear'}` 
+                        ]
+                    }
+                })
+            })
+            // Check if we collided
+            if(player.collided){
+                resetPlayer()
+            }
+            return newStage;
+        }
+
+        setStage(prev => updateStage(prev))
+    }, [player, resetPlayer])
+
+    return [stage, setStage]
 }

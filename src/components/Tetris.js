8b7084f27290a62b81b8529d7852b4a95160ffaa
Shane Slone <slone.shane@gmail.com>
2020-10-21 15:01:53 -0400

    added rotation

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -18,7 +18,7 @@ export default function Tetris() {
     const [dropTime, setDropTime] = useState(null)
     const [gameOver, setGameOver] = useState(false)
 
-    const [player, updatePlayerPos, resetPlayer] = usePlayer()
+    const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer()
     const [stage, setStage] = useStage(player, resetPlayer)
 
 
@@ -61,6 +61,8 @@ export default function Tetris() {
                 movePlayer(1)
             } else if(keyCode === 40) {
                 dropPlayer()
+            } else if(keyCode === 38) {
+                playerRotate(stage, 1)
             }
         }
     }
